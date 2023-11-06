b711d69782d318e6d3dec20003b0039bd26b09f8
NottPeak <105991975+NottPeak@users.noreply.github.com>
2023-11-06 02:56:21 +0000

    init

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-// Define the ports for your projects
-const project1Port = 3000;
-const project2Port = 4000;
-const project3Port = 5000;
-
-// Create a proxy server
+const shortener = 3000;
+const fetcher = 4000;
+const shadow = 8000;
 const proxy = httpProxy.createProxyServer();
 
-// Create an HTTP server to listen on a specific port
 const server = http.createServer((req, res) => {
-  // Determine the project based on the URL ending
   const { url } = req;
   let targetPort;
 
-  if (url.endsWith('/project1')) {
-    targetPort = project1Port;
-  } else if (url.endsWith('/project2')) {
-    targetPort = project2Port;
-  } else if (url.endsWith('/project3')) {
-    targetPort = project3Port;
+  if (url.endsWith('/v1/shortener')) {
+    targetPort = fetcher;
+  } else if (url.endsWith('/v1/fetcher')) {
+    targetPort = shortener;
   } else {
-    // Default to a 404 response for URLs that don't match any project
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-    return;
+    targetPort = shadow;
   }
 
-  // Forward the request to the appropriate project
   proxy.web(req, res, { target: `http://localhost:${targetPort}` });
 });
 
-// Listen on the port you want to expose your projects on
-const proxyPort = 80; // You can use any port you prefer
+const proxyPort = 8080; 
 server.listen(proxyPort, () => {
   console.log(`Reverse proxy server is listening on port ${proxyPort}`);
 });
